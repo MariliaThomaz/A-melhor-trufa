e0fdea3a0a839cdf6118764243bfb3944b864c36
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2023-01-06 18:53:57 -0300

    add  propriedade a model ProdutoModel

--- A_MELHOR_TRUFA.Domain/Models/Produtos/ProdutoModel.cs
+++ A_MELHOR_TRUFA.Domain/Models/Produtos/ProdutoModel.cs
@@ -8,5 +8,18 @@ namespace A_MELHOR_TRUFA.Domain.Models.Produtos
 {
     public class ProdutoModel
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; }
+        public DateTime DataVencimento { get; set; }
+        public DateTime  DataFabricacao { get; set; }
+        public  decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+
+
+        
+
+
     }
 }
