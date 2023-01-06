74d31d1f5bbe64e9ed6e9b67778efc0f41433825
José Esbellute <jose.esbellute@gmail.com>
2023-01-06 18:22:58 -0300

    Estrutura de referencia de projetos da aplicação

---
+++ A_MELHOR_TRUFA.Service/Service/Forms/Login/LoginService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_MELHOR_TRUFA.Service.Service.Forms.Login
+{
+    public class LoginService
+    {
+    }
+}
